5989c95a5e1cd676a94ccfa676cb7b3b5b5f18fc
abhis007 <abi007in@gmail.com>
2020-08-25 00:23:33 +0530

    inital commit

---
+++ pages/home.vue
@@ -0,0 +1,289 @@
+<template>
+    <div class=" ">
+      <vs-navbar target-scroll="#hide-scroll-content" square hide-scroll leftCollapsed primary  text-white  v-model="active">
+        <template #left>
+         <b>AUTHOUR.COM</b>
+        </template>
+        <vs-navbar-item :active="active == 'guide'" id="guide">
+         Home
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          Novels
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'components'" id="components">
+          Short Stories
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'license'" id="license">
+          Poems
+        </vs-navbar-item>
+         <vs-navbar-item :active="active == 'license'" id="license">
+           Articles
+        </vs-navbar-item>
+        <template #right>
+          <vs-button flat >Login</vs-button>
+          <vs-button>Get Started</vs-button>
+        </template>
+      </vs-navbar>
+      <div id="hide-scroll-content" class="square">
+          <div class="content">
+       <vs-row vs-justify="center">
+        <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+<div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+            <div w="3" style="padding:5px">
+              <vs-card>
+    <template #title>
+      <h3>Pot with a plant</h3>
+    </template>
+    <template #img>
+      <img src="/foto5.png" alt="">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
+
+            </div>
+
+  </vs-row>
+          </div>
+      </div>
+    </div>
+  </template>
+ 
+
+
+  <script>
+  export default {
+    data:() => ({
+      active: 'guide'
+    })
+  }
+  </script>
+
